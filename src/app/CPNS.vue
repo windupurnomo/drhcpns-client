d197353c5588c438772e250934898d97f607b7a5
Windu Purnomo <wpwindupurnomo@gmail.com>
2018-12-08 17:12:38 +0700

    initial commit

---
+++ src/app/CPNS.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <b-table :items="cpnsList" :fields="fields"></b-table>
+</template>
+
+
+<script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        cpnsList: [],
+        fields: ['nik', 'name', 'address']
+      }
+    },
+    methods: {
+      getCpns(){
+        axios.get("http://localhost:8080/cpns")
+        .then(response => this.cpnsList = response.data);
+      }
+    },
+    mounted() {
+      this.getCpns();
+    }
+  }
+</script>
